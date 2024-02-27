3e1bbbe9831afc77410164a2af36577eb375ff76
rayoo22 <ryankibet14@gmail.com>
2024-02-27 11:13:39 -0800

    creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a node
+ * @parent: pointer to parent
+ * @value: value for node created
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nodo;
+
+	nodo = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (nodo == NULL)
+	{
+		return (NULL);
+	}
+	nodo->n = value;
+	nodo->parent = parent;
+	nodo->left = NULL;
+	nodo->right = NULL;
+	return (nodo);
+}

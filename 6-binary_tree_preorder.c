b49d3747aaaadf5dd94bc4da32a08389e80469b7
rayoo22 <ryankibet14@gmail.com>
2024-02-27 12:05:30 -0800

    a binary tree using preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_tree_preorder.c"
+
+/**
+ * binary_tree_preorder - a binary tree using preorder traversal
+ * @tree: a pointer to the root node of the tree
+ * @func: a pointer tot a function to call for each node
+ * Otherwise 0
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree-right, func);
+}
